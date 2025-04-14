7008b8529f694a74c507c33f7c411f9420ad5877
PranaliPophale <169164580+PranaliPophale@users.noreply.github.com>
2025-04-14 19:27:52 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,15 @@
+public class Product {
+    int id;
+    String name;
+    double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println(id + ". " + name + " - ₹" + price);
+    }
+}
